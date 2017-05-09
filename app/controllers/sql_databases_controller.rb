7bbe01ed29673c1bc6227309fdccd60283afb911
Tran Huu Minh <minhth1905@gmail.com>
2017-05-09 09:30:56 +0700

    seed-data

--- app/controllers/sql_databases_controller.rb
+++ app/controllers/sql_databases_controller.rb
@@ -2,7 +2,7 @@ class SqlDatabasesController < ApplicationController
 
   def index
     client = Mysql2::Client.new(:host => "localhost", :username => "root",
-      :password => "cubin1905", :database => "monika")
+      :password => "cubin1905", :database => "orientdb_client")
     results = client.query("SELECT * FROM users")
   end
 end
